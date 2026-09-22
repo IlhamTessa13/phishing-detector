eaa90c2f24351c6a207a396c2e7df221a9a728a7
IlhamTessa13 <ilhamts139@gmail.com>
2026-09-22 12:20:28 +0700

    commitlagii

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { PredictionResponse } from "../types/prediction";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = "https://web-phishing-datmin-production.up.railway.app";
 
 export const checkUrl = async (url: string): Promise<PredictionResponse> => {
   const response = await axios.post<PredictionResponse>(
