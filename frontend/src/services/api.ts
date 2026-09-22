1d0f30f6098a80bfa3d2710e843b419440524d1c
IlhamTessa13 <ilhamts139@gmail.com>
2026-09-22 11:46:30 +0700

    commit terus

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { PredictionResponse } from "../types/prediction";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export const checkUrl = async (url: string): Promise<PredictionResponse> => {
   const response = await axios.post<PredictionResponse>(
